16e2b9d069a2c15ad3d9d8b576051ca97fdf8594
NagarajTantri <nagarajtantri@gmail.com>
2021-12-14 14:15:01 +0530

    Added factory samples

--- src/main/java/org/java/sample/tutorials/designpatterns/factory/ApiHandlerFactory.java
+++ src/main/java/org/java/sample/tutorials/designpatterns/factory/ApiHandlerFactory.java
@@ -1,2 +1,19 @@
-package org.java.sample.tutorials.designpatterns.factory;public class ApiHandlerFactory {
+package org.java.sample.tutorials.designpatterns.factory;
+
+import org.java.sample.tutorials.designpatterns.factory.handlers.AdvertiserApiHandler;
+import org.java.sample.tutorials.designpatterns.factory.handlers.ApiHandler;
+import org.java.sample.tutorials.designpatterns.factory.handlers.CampaignApiHandler;
+import org.java.sample.tutorials.designpatterns.factory.validators.AdvertiserValidator;
+import org.java.sample.tutorials.designpatterns.factory.validators.CampaignValidator;
+
+public class ApiHandlerFactory {
+  public static ApiHandler getApiHandler(String apiType) {
+    if (apiType.equals("Advertiser")) {
+      AdvertiserValidator advertiserValidator = new AdvertiserValidator();
+      return new AdvertiserApiHandler(advertiserValidator);
+    } else {
+      CampaignValidator campaignValidator = new CampaignValidator();
+      return new CampaignApiHandler(campaignValidator);
+    }
+  }
 }
--- src/main/java/org/java/sample/tutorials/designpatterns/factory/RequestExecutionerFactoryExample.java
+++ src/main/java/org/java/sample/tutorials/designpatterns/factory/RequestExecutionerFactoryExample.java
@@ -1,2 +1,11 @@
-package org.java.sample.tutorials.designpatterns.factory;public class RequestExecutionerFactoryExample {
+package org.java.sample.tutorials.designpatterns.factory;
+
+import org.java.sample.tutorials.designpatterns.factory.handlers.ApiHandler;
+
+public class RequestExecutionerFactoryExample {
+
+  public void handleApiRequests(String apiType, String requestBody) {
+    ApiHandler apiHandler = ApiHandlerFactory.getApiHandler(apiType);
+    apiHandler.handleRequest(requestBody);
+  }
 }
